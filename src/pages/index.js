db2319cfb2d98ec301855dd8ba95d099e4dbee7e
Manuwu <manuexposito82@outlook.com>
2021-06-21 17:13:09 -0300

    deleted almost everything in pages/index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,3 @@
-import React, {useState} from 'react'
-import {BrowserRouter as Router} from 'react-router-dom';
-import Sidebar from '../components/sidebar';
-import Navbar from '../components/navbar/index.js';
 
-function Home() {
-  const [isOpen, setIsOpen] = useState(false); // useState returns a 
-
-  const toggle = () =>{
-    setIsOpen(!isOpen);
-  }
-
-  return (
-    <Router>
-      <Sidebar isOpen={isOpen} toggle={toggle} />
-      <Navbar toggle={toggle}/>
-    </Router>
-  )
-}
 
 export default Home
